65371cdeca4ee0138a8ec8bc69ff0394220d3aa9
Vania2007 <momotis2007@gmail.com>
2024-04-09 21:01:14 +0300

    update-age

--- age/age.js
+++ age/age.js
@@ -8,24 +8,24 @@ function birthDateToAge(b) {
   let strYears = "років";
   if (age % 10 === 1 && age !== 11) {
     strYears = "рік";
-  } else if (age % 10 < 4 && (age < 11 || age > 14)) {
+  } else if (age % 10 < 4 && (age < 11 || age > 14) && age !== 0) {
     strYears = "роки";
   }
+
   let countOfMonth;
   if (n.getMonth() < b.getMonth()) {
-    countOfMonth = (n.getMonth() + 12) - b.getMonth();
+    countOfMonth = n.getMonth() + 12 - b.getMonth();
   } else {
     countOfMonth = n.getMonth() - b.getMonth();
   }
-
   let strMonth = "місяців";
-  if (countOfMonth % 10 === 1 && age !== 11) {
-    strYears = "місяць";
-  } else if (age % 10 < 4 && age < 11) {
-    strYears = "місяці";
+  if (countOfMonth % 10 === 1 && countOfMonth !== 11) {
+    strMonth = "місяць";
+  } else if (countOfMonth % 10 < 5 && countOfMonth < 10 && countOfMonth !== 0) {
+    strMonth = "місяці";
   }
   let str = `Вам ${age} ${strYears}<br/>І ${countOfMonth} ${strMonth}`;
   return str;
 }
 
-document.write(birthDateToAge("2007-09-13"));
+document.write(birthDateToAge("2015-06-04"));
